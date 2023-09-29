547ae6de094122c22fdf10239e2e285339c2cfe0
erardlucien <erardlaurent1@gmail.com>
2023-09-29 03:33:00 +0200

    keyup is no longer needed.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,13 @@ let writeSomething = document.querySelector('#write-something');
 let showWrittenText = document.querySelector('.show-written-text');
 let resetButton = document.querySelector('#reset-button');
 
-writeSomething.addEventListener('keyup', () => {
-    showWrittenText.textContent = writeSomething.value;
-});
-
 resetButton.addEventListener('click', () => {
     showWrittenText.textContent = writeSomething.value = '';
 });
+
+(
+    function checking () {
+        showWrittenText.textContent = writeSomething.value;
+        setTimeout(checking, 10);
+    }
+)();
